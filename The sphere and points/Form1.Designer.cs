5884c23b0d823d1c235d8976ae5dcf7aefb91e02
Moskalenko Sergey <oknelaksoms@gmail.com>
2020-06-28 12:41:33 +0300

    version 0.1

---
+++ The sphere and points/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace The_sphere_and_points
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(581, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "1";
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(12, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(34, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "1000";
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(619, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "- multiplies by";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "- num of points";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(97, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(697, 463);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.SizeChanged += new System.EventHandler(this.PictureBox1_SizeChanged);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseUp);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.LargeChange = 100;
+            this.trackBar1.Location = new System.Drawing.Point(83, 6);
+            this.trackBar1.Maximum = 50000;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(492, 43);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(744, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "- remove";
+            this.label3.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.ForeColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(706, 10);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(32, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "0";
+            this.textBox3.Visible = false;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBar2.AutoSize = false;
+            this.trackBar2.LargeChange = 1;
+            this.trackBar2.Location = new System.Drawing.Point(12, 94);
+            this.trackBar2.Maximum = 5000;
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(43, 316);
+            this.trackBar2.TabIndex = 9;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar2.Value = 1000;
+            this.trackBar2.Scroll += new System.EventHandler(this.TrackBar2_Scroll);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Black;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox1.ForeColor = System.Drawing.Color.White;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(2, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(83, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.Location = new System.Drawing.Point(9, 413);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 97);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBar3.AutoSize = false;
+            this.trackBar3.LargeChange = 1;
+            this.trackBar3.Location = new System.Drawing.Point(97, 194);
+            this.trackBar3.Maximum = 5;
+            this.trackBar3.Minimum = -5;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar3.Size = new System.Drawing.Size(43, 316);
+            this.trackBar3.TabIndex = 12;
+            this.trackBar3.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar3.Visible = false;
+            this.trackBar3.Scroll += new System.EventHandler(this.TrackBar3_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBar4.AutoSize = false;
+            this.trackBar4.LargeChange = 1;
+            this.trackBar4.Location = new System.Drawing.Point(146, 194);
+            this.trackBar4.Maximum = 5;
+            this.trackBar4.Minimum = -5;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar4.Size = new System.Drawing.Size(43, 316);
+            this.trackBar4.TabIndex = 13;
+            this.trackBar4.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar4.Visible = false;
+            this.trackBar4.Scroll += new System.EventHandler(this.TrackBar4_Scroll);
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBar5.AutoSize = false;
+            this.trackBar5.LargeChange = 1;
+            this.trackBar5.Location = new System.Drawing.Point(195, 194);
+            this.trackBar5.Maximum = 5;
+            this.trackBar5.Minimum = -5;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar5.Size = new System.Drawing.Size(43, 316);
+            this.trackBar5.TabIndex = 14;
+            this.trackBar5.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar5.Visible = false;
+            this.trackBar5.Scroll += new System.EventHandler(this.TrackBar5_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(794, 519);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.TrackBar trackBar5;
+    }
+}
+
---
+++ The sphere and points/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_sphere_and_points
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Point3D a, b, c;
+        Graphics g;
+        Bitmap bmp;
+        void draw2D(int num_of_points, double mult)
+        {
+            //bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            //pictureBox1.Refresh();
+            //g = Graphics.FromImage(bmp);
+            g.Clear(Color.Black);
+            int m = min(pictureBox1.Width, pictureBox1.Height);//диаметр
+            g.DrawEllipse(Pens.Black, 0, 0, m, m);
+            //int R = pictureBox1.Width;
+            int x1, x2, y1, y2;
+            for (int i = 0; i <= num_of_points; i++)
+            {
+                x1 = (m / 2) + Convert.ToInt32((m / 2) * Math.Cos(i * 2 * Math.PI / num_of_points));
+                y1 = (m / 2) + Convert.ToInt32((m / 2) * Math.Sin(i * 2 * Math.PI / num_of_points));
+                x2 = (m / 2) + Convert.ToInt32((m / 2) * Math.Cos(i * mult * 2 * Math.PI / num_of_points));
+                y2 = (m / 2) + Convert.ToInt32((m / 2) * Math.Sin(i * mult * 2 * Math.PI / num_of_points));
+                g.DrawLine(Pens.White, x1, y1, x2, y2);
+            }
+            pictureBox1.Image = bmp;
+        }
+        int min(int a, int b)
+        {
+            if (a < b)
+                return a;
+            else
+                return b;
+        }
+        void draw3D()
+        {
+            g.Clear(Color.Black);
+            int m = min(pictureBox1.Width, pictureBox1.Height);
+            int a1 = trackBar2.Value*2/3, a2 = trackBar2.Value/3;
+            double k = Convert.ToDouble(textBox1.Text);
+            double l = Convert.ToDouble(textBox1.Text);//множетили
+            //a1 = 180;
+            d3 p1, p2;
+            for(int i=0;i<a1;i+=10)
+            {
+                for(int j=0;j<a2;j+=10)
+                {
+                    p1 = new d3((m / 2) * Math.Sin(i * 2 * Math.PI / a1) * Math.Sin(j * 2 * Math.PI / a2),
+                        (m / 2) * Math.Cos(i * 2 * Math.PI / a1) * Math.Sin(j * 2 * Math.PI / a2),
+                        (m / 2) * Math.Cos(j * 2 * Math.PI / a2));
+                    p2 = new d3((m / 2) * Math.Sin(i * 2 * k * Math.PI / a1) * Math.Sin(j * 2 * l * Math.PI / a2),
+                        (m / 2) * Math.Cos(i * 2 * k * Math.PI / a1) * Math.Sin(j * 2 * l * Math.PI / a2),
+                        (m / 2) * Math.Cos(j * 2 * l * Math.PI / a2));
+                    d3.draw_line(p1,p2,pictureBox1,g);
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+        private void PictureBox1_SizeChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "2D")
+                draw2D(Convert.ToInt32(textBox2.Text), Convert.ToDouble(textBox1.Text));
+            else if (comboBox1.Text == "3D")
+            {
+                draw3D();
+            }
+        }
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            textBox1.Text = (trackBar1.Value * (0.001)).ToString();
+        }
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //pictureBox1.Refresh();
+            if (comboBox1.Text == "2D")
+                draw2D(Convert.ToInt32(textBox2.Text), Convert.ToDouble(textBox1.Text));
+            else if (comboBox1.Text == "3D")
+            {
+                //draw3D();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            comboBox1.Items.Add("2D");
+            comboBox1.Items.Add("3D");
+            comboBox1.Text = "2D";
+            //
+            d3.reset();
+            //
+        }
+        
+        private void TrackBar2_Scroll(object sender, EventArgs e)
+        {
+            textBox2.Text = trackBar2.Value.ToString();
+        }
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "2D")
+                draw2D(Convert.ToInt32(textBox2.Text), Convert.ToDouble(textBox1.Text));
+            else if (comboBox1.Text == "3D") { }
+                //draw3D();
+        }
+
+        Point start,end;
+        bool is_mouse_down = false,f=false;
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            is_mouse_down = true;
+            //timer1.Enabled = true;
+            start = new Point(e.X, e.Y);
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1)
+            {
+                g.Clear(Color.Black);
+                d3.reset();
+                d3.rotate(trackBar3.Value, trackBar4.Value, trackBar5.Value);
+                //pictureBox1.Image = bmp;
+                draw3D();
+            }
+        }
+
+        private void TrackBar5_Scroll(object sender, EventArgs e)
+        {
+            //Point3D.a_OX = trackBar5.Value *( Math.PI / 180);
+            //draw3D();
+        }
+
+        private void TrackBar4_Scroll(object sender, EventArgs e)
+        {
+            //Point3D.a_OY = trackBar4.Value * (Math.PI / 180);
+            //draw3D();
+        }
+
+        private void TrackBar3_Scroll(object sender, EventArgs e)
+        {
+            //Point3D.a_OZ = trackBar3.Value * (Math.PI / 180);
+            //draw3D();
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex==0)
+            {
+                trackBar3.Visible = false;
+                trackBar4.Visible = false;
+                trackBar5.Visible = false;
+                timer1.Enabled = false;
+            }
+            else
+            {
+                trackBar3.Visible = true;
+                trackBar4.Visible = true;
+                trackBar5.Visible = true;
+                timer1.Enabled = true;
+            }
+        }
+
+        //int ex, ey;
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(is_mouse_down)
+            {
+                //if (f)
+                //    end = new Point(ex, ey);
+                //else
+                //    start = new Point(ex, ey);
+                //f = !f;
+
+
+                //end = new Point(e.X, e.Y);
+                //if (start.X - end.X > 0)
+                //    if (start.Y - end.Y > 0)
+                //        p[0].Rotate(0.01, 0.01);
+                //    else
+                //        p[0].Rotate(0.01, -0.01);
+                //else
+                //    if (start.Y - end.Y > 0)
+                //    p[0].Rotate(-0.01, 0.01);
+                //else
+                //    p[0].Rotate(-0.01, -0.01);
+
+                //label4.Text = (Point3D.a_XY).ToString() + " - XY\n" + (Point3D.a_XZ).ToString() + " - XZ\n" + (Point3D.a_YZ).ToString() + " - YZ";
+            }
+            if (comboBox1.Text == "2D")
+                draw2D(Convert.ToInt32(textBox2.Text), Convert.ToDouble(textBox1.Text));
+            else if (comboBox1.Text == "3D")
+            { }// draw3D();
+        }
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            is_mouse_down = false;
+        }
+    }
+}
